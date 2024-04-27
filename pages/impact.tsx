6b94be31037c351e94b91d53b340faef21e452e0
21givenchy <georgekarani44@gmail.com>
2024-04-27 10:20:50 +0300

    yoo new suff

--- pages/impact.tsx
+++ pages/impact.tsx
@@ -5,6 +5,10 @@ const activities = [
   { name: '10 min shower', impacts: ['SDG 6'] },
   { name: '1 hour walk', impacts: ['SDG 3', 'SDG 11'] },
   { name: '1$ donation', impacts: ['SDG 1', 'SDG 2', 'SDG 3', 'SDG 4', 'SDG 5', 'SDG 6', 'SDG 7', 'SDG 8', 'SDG 9', 'SDG 10', 'SDG 11', 'SDG 12', 'SDG 13', 'SDG 14', 'SDG 15', 'SDG 16', 'SDG 17'] },
+  { name: 'Recycle', impacts: ['SDG 12'] },
+  { name: 'Turn off unused bulbs', impacts: ['SDG 7'] },
+  { name: 'Cycle', impacts: ['SDG 3', 'SDG 11'] },
+  { name: 'Plant trees', impacts: ['SDG 15'] },
   // Add more activities as needed
 ];
 
